e24853a87c4759d66034f39948f26023b0862183
mhkmhsh <mahekmahesh12@gmail.com>
2024-01-23 10:24:04 -0500

    DONEE

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 
-function calculateAndDisplay()
-{
-    let bill = parsefloat(document.getElementById('totalBill').value)
-    let numOfPpl = parselnt( document.getElementById('numOfPeople').value)
+function calculateAndDisplay() {
+    let bill = parseFloat(document.getElementById('totalBill').value)
+    let numOfPpl = parseInt(document.getElementById('numOfPpl').value)
     let serviceQuality = document.getElementById('serviceQuality').value
 
     let tip = calculateTip(bill, serviceQuality)
     let totalBill = calculateTotalBill(bill, tip)
     let amtPerPers = calculateAmtPerPers(totalBill, numOfPpl)
+
+
+// Display the results in the HTML document
+document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
+document.getElementById('totalBillResult').innerText =
+    'Total Bill: $' + totalBill.toFixed(2)
+document.getElementById('amtPerPersonResult').innerText =
+    'Amount per person: $' + amtPerPers.toFixed(2)
+
+}
+// helper func to calc tip based on bill and serv quality
+function calculateTip(bill, quality) {
+    if (quality === 'Great') {
+        return bill * .2
+    } else if (quality === 'Good') {
+        return bill * .15
+    } else if (quality === 'Poor') {
+        return bill * .10
+    } else {
+        return 0
+    }
 }
 
- // Display the results in the HTML document
- document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
- document.getElementById('totalBillResult').innerText =
-   'Total Bill: $' + totalBill.toFixed(2)
- document.getElementById('amtPerPersonResult').innerText =
-   'Amount per person: $' + amtPerPers.toFixed(2)
-
-
-
-
-
-
+function calculateTotalBill(bill, tip) {
+    return bill + tip
+}
+function calculateAmtPerPers(totalBill, numOfPeople) {
+    return totalBill / numOfPeople
+}
 
 /* let bill = 120
 let numOfPpl = 5
